e463bc0d9912414018b3887463f1ceb2e9489956
Alexandre Machado <amch.smd@gmail.com>
2024-08-23 20:42:09 -0300

    style: create fonts utility and global stylesheet

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Raleway, Montserrat } from 'next/font/google';
+
+export const raleway = Raleway({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-raleway',
+});
+
+export const montserrat = Montserrat({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-montserrat',
+});
